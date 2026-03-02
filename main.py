b8f9dd253669b1ac204e0895da4f7a5842237593
Nyatari <nagakuku@outlook.com>
2026-03-02 02:09:27 +0100

    First Upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "PasteURLHere"
+filetype = ".mp3"
+downloadRange = range(1, 114)
+
+# Change writing mode accordingly
+# "w"   write (text mode), ex: txt, json
+# "wb"  write (binary mode), ex: mp3, png, pdf
+writingMode = "wb"
+
+for i in downloadRange:
+    urlfullPath = url + str(i).zfill(3) + filetype
+    response = requests.get(urlfullPath)
+    with open("/download" + str(i).zfill(3) + filetype, writingMode) as f:
+        f.write(response.content)
